f36bc121adc2e34247935adea7d80ec06fe3fc7a
Dhruviika <e21cseu0292@bennett.edu.in>
2024-06-01 19:04:29 +0530

    Intiliased schema.prisma file and performed migrations.

---
+++ backend/prisma/migrations/20240529013358_added/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Profile" ALTER COLUMN "bio" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT NOT NULL;
