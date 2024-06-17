8c82822ed25b7ea8e90c7fe8f52e1b28957cff07
Dhruviika <e21cseu0292@bennett.edu.in>
2024-06-17 21:32:49 +0530

    made signup and signin grids responsive.

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,7 +3,7 @@ import { AuthComponent } from "../components/Auth";
 
 export const Signin = () => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <AuthComponent type="signin" />
       <div className=" invisible lg:visible">
         <QuoteComponent />
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,7 +3,7 @@ import { AuthComponent } from "../components/Auth";
 
 export const Signup = () => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <AuthComponent type="signup" />
       <div className=" invisible lg:visible">
         <QuoteComponent />
