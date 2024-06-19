681e4b1bb70ea46f71b8b0393e1cb6b82b9673b6
Dhruviika <e21cseu0292@bennett.edu.in>
2024-06-19 11:56:14 +0530

    Added a new-story page for user to write blog.

--- frontend/src/pages/NewStory.tsx
+++ frontend/src/pages/NewStory.tsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
 import { Navbar } from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const NewStory = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+
+  const navigate = useNavigate();
+
+  const handlePublish = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const res = await axios.post(
+        "https://backend.cornstar.workers.dev/api/v1/blog/post",
+        {
+          title,
+          content,
+        },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      if (res.status === 200) {
+        navigate("/me");
+      }
+    } catch (e) {
+      console.error("An error occurred while publishing the story", e);
+    }
+  };
 
   return (
     <div>
@@ -22,8 +50,11 @@ export const NewStory = () => {
           onChange={(e) => setContent(e.target.value)}
         />
       </div>
-      <div className="flex justify-center items-center h-40 lg:hidden">
-        <button className="bg-orange-500 text-white rounded-full py-3 w-1/4 hover:bg-orange-700">
+      <div className="flex justify-center items-center h-40">
+        <button
+          onClick={handlePublish}
+          className="bg-orange-500 text-white rounded-full py-3 w-1/4 hover:bg-orange-700"
+        >
           Publish
         </button>
       </div>
