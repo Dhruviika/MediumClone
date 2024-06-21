3ce407308648a8f9a28add906fc803f3bfe59f5b
Dhruviika <e21cseu0292@bennett.edu.in>
2024-06-21 17:59:00 +0530

    changed blog to home.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../components/Navbar";
 import { StepInto } from "../components/StepInto";
 import { CustomCategory } from "../components/CustomCategory";
 
-export const Blog = () => {
+export const Home = () => {
   return (
     <div className="bg-orange-bg bg-cover bg-center h-svh lg:h-svh">
       <Navbar />
