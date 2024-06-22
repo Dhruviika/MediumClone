109c15371f93b6210ccb7a4110820a5594f37300
Dhruviika <e21cseu0292@bennett.edu.in>
2024-06-22 17:46:31 +0530

    seperately added authentication to routes.

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -7,7 +7,23 @@ export const authenticateRoute = new Hono<{
   };
 }>();
 
-authenticateRoute.use("/*", async (c, next) => {
+authenticateRoute.use("/blog/*", async (c, next) => {
+  const jwt = c.req.header("Authorization");
+  if (!jwt) {
+    c.status(401);
+    return c.json({ message: "unauthorized" });
+  }
+
+  const payload = await verify(jwt, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ message: "unauthorized" });
+  }
+  c.set("jwtPayload", payload.id as string);
+  await next();
+});
+
+authenticateRoute.use("/user/me", async (c, next) => {
   const jwt = c.req.header("Authorization");
   if (!jwt) {
     c.status(401);
